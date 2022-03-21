ad6caa15b283e8458526a37c8d25b083f333ad4e
CharlieRojas <crc_185@yahoo.com>
2022-03-21 18:57:05 -0500

    commit tree cf7762bc5240f7616c65438d75d610a7d68d79ce

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((books) => renderBooks(books));
+};
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -10,7 +11,7 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
-}
+};
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
